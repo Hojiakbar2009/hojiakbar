26e5e623209ee5cdf7cd1d17ff8ef023f43b28b5
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-07-11 14:07:20 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Main } from "./components/Main";
+import { Jamshid } from "./pages/Jamshid/Jamshid";
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../components/Main.css"
+import { Jamshid } from "../pages/Jamshid/Jamshid";
+
+export function Main(){
+    return(
+        <div className="main">
+            <Jamshid/>
+        </div>
+    )
+}
